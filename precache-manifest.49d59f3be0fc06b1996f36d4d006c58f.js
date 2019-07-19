6f5afc7c59d1eb6c48451f406283979b889ead46
harpreetkhalsagtbit <harpreetkhalsagtbit@gmail.com>
2019-07-20 01:42:11 +0530

    Updates

---
+++ precache-manifest.49d59f3be0fc06b1996f36d4d006c58f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e003d99b1e1405fa33759c13917ae333",
+    "url": "/react-swapi/index.html"
+  },
+  {
+    "revision": "6bba979691dc1578e8bb",
+    "url": "/react-swapi/static/css/main.c7cace19.chunk.css"
+  },
+  {
+    "revision": "ef7ac7c0457525ab0482",
+    "url": "/react-swapi/static/js/2.aec14cca.chunk.js"
+  },
+  {
+    "revision": "6bba979691dc1578e8bb",
+    "url": "/react-swapi/static/js/main.5be8305e.chunk.js"
+  },
+  {
+    "revision": "7a1016b3ff399e7649f0",
+    "url": "/react-swapi/static/js/runtime~main.d8b3eecc.js"
+  }
+]);
